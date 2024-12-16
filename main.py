42f8d99b193f11d2b14e3f8037411df43103b0be
Rodrigo Vittori <vittorirodrigo@gmail.com>
2024-12-16 20:23:28 -0300

    [M6.L1] · Actividad #2: "Función on_mouse_move()
    Agregamos movimiento de la nave con el mouse

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 #pgzero
 
-# M6.L1: Actividad #1: "Viaje al espacio"
-
-"""
+""" > [M6.L1] · Actividad #2: "Función on_mouse_move()"
 
 Kenney assets:
 
@@ -10,13 +8,12 @@ Kodland: https://kenney.nl/assets/space-shooter-extension
 Extra: https://kenney.nl/assets/space-shooter-redux
 Planetas: https://kenney.nl/assets/planets
 UI: https://kenney.nl/assets/ui-pack-sci-fi
-
 """
 
 WIDTH = 600
 HEIGHT = 450
 
-TITLE = "GUERRA GALACTICA"
+TITLE = "GUERRA GALÁCTICA"
 FPS = 30
 
 # Objetos y Variables
@@ -26,5 +23,11 @@ fondo = Actor("space")
 # Funciones PGZERO
 def draw():
   fondo.draw()
-  nave.draw()
+  texto_temp = "Coord: (x: " + str(int(nave.x)) + ", y: " + str(int(nave.y)) + ")"
+  screen.draw.text(texto_temp, midleft=(20, 20), color = "white", fontsize = 24)
+
   screen.draw.text(TITLE, center=(300, 100), color="white", background="black")
+  nave.draw()
+    
+def on_mouse_move(pos):
+  nave.pos = pos
